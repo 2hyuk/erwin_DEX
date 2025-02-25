4bf3e51223a090f27224aee7731d775533917208
박의혁 <erwin@bag-uihyeog-ui-MacBookPro.local>
2025-02-26 01:41:37 +0900

    simple_update_like_UniSwapV1

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -3,55 +3,74 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TokenA is ERC20 {
-    
-    constructor() ERC20("TokenA", "A") {
-        _mint(msg.sender, 50 * 10)
-    }
-
-}
 
 contract Exchange is ERC20 {
-    // Ether <> A Token Exchange
-    address public tokenAddress;  // = 0x...
+    address tokenAddress;
+    constructor() ERC20("Uniswap V1", "UNI-V1") {}
 
-    constructor(address _tokenAddress) ERC20("Uniswap LP", "UNI-LP") {
-        tokenAddress = _tokenAddress;
-    }
-
-    function addLiquidity() public payable {
-        // LP 사용자로부터 이더, 토큰을 받음
-        uint etherAmount = msg.value;
-        uint tokenAmount = etherAmount;
-        TokenA tokenContract = TokenA(tokenAddress);
-        tokenContract.transferFrom(msg.sender, address(this), tokenAmount);
-        // LP 사용자에게 받은 만큼의 LP 토큰을 민팅함.
-        _mint(msg.sender, tokenAmount);
-    }
+    function addLiquidity() public payable  {}
 
     function removeLiquidity(uint lpTokenAmount) public {
-        // 1. LP토큰을 burn한다
-        _burn(msg.sender, lpTokenAmount);
-        uint etherAmount = lpTokenAmount;
+        // 지분율 = lpTokenAmount / 전체 발행된 lpTokenAmount
+        uint laShares = lpTokenAmount / balanceOf(address(this));
+        _burn(msg.sender, lpTokenAmount); // 소각
+
+        // 총 Ether의 갯수 * 지분율
+        uint etherAmount = address(this).balance * lpTokenAmount / balanceOf(address(this));
+
+        // 총 token의 갯수 * 지분율
+        ERC20 token = ERC20(tokenAddress);
+        uint tokenAmount = token.balanceOf(address(this)) * lpTokenAmount / balanceOf(address(this));
+
         payable(msg.sender).transfer(etherAmount);
-        TokenA tokenContract = TokenA(tokenAddress);
-        tokenContract.transfer(msg.sender, tokenAmount);
-        // 2. 지분만큼 이더, 토큰을 반환한다.
+        token.transfer(msg.sender, tokenAmount);
     }
 
-    function etherToTokenSwap() public payable{
+    function etherToTokenInput(uint minTokens) public payable {
         uint etherAmount = msg.value;
-        //CSMM: ehterAmount + tokenAmont = k
-        uint tokenAmount = etherAmount;
-        TokenA tokenContract = TokenA(tokenAddress);
-        tokenContract.transfer(msg.sender, tokenAmount);
+        ERC20 token = ERC20(tokenAddress);
+        uint tokenAmount = getInputPrice(
+            etherAmount,
+            address(this).balance - msg.value,  // 컨트랙트 호출하고 여기서 연산하는 시점에 이미 이더를 보냈기에, msg.value 빼줘야 풀 계수 유지됨
+            token.balanceOf(address(this))
+        );
+        require(tockenAmount >= mintTokens);
+        token.transfer(msg.sender, tokenAmount);
     }
 
-    function tokenToEtherSwap(uint tokenAmount) {
-        TokenA tokenContract = TokenA(tokenAddress);
-        tokenContract.transferFrom(msg.sender, address(this), tokenAmount);
-        uint etherAmount = tokenAmount;
-        payable(msg.sender).tranasfer(etherAmount);
+    // getInputPrice == getOutputAmount , 유니스왑에서 차용
+    function getInputPrice(
+        uint inputAmount,
+        uint inputReserve,
+        uint outputReserve
+    ) public pure returns (uint outputAmount){
+        return inputAmount * 997 / 1000;
     }
+
+    function getOutputPrice(
+        uint inputAmount,
+        uint inputReserve,
+        uint outputReserve
+    ) public pure returns (uint inputAmount){
+        return outputAmount / 997 * 1000;
+    }
+
+    function etherToTokenOutput(
+        uint tokenAmount,
+        uint maxEtherAmount
+    ) public payable {
+        uint etherAmount = tokenAmount / 997 * 1000;  //price impact, slippage 계산 못하니 유저가 넉넉히 보내는 것
+
+        require(msg.value >= etherAmount);
+        require(maxEtherAmount >= etherAmount);
+        uint etherRefundAmount = msg.value - etherAmount;
+        if (etherRefundAmount > 0) {
+            payable(msg.sender).transfer(etherRefundAmount);
+        }
+        ERC20 token = ERC20(tokenAddress);
+        token.transfer(msg.sender, tokenAmount);
+
+        
+    }
+
 }
-
